4d58663c859c339b931b088ef76ac108f803353f
Aaron Merino <lopez.aaron1022@gmail.com>
2021-11-25 18:50:23 -0600

    Update 2.asm

--- 2.asm
+++ 2.asm
@@ -1,26 +1,33 @@
 .data
     str1: .asciiz "input palabra: "
+    str2: .asciiz "Cantidad de vocales: "
     inputStr: .asciiz ""
-    result: .asciiz "Cantidad de vocales: "
 .text
 .globl main
 
+countVocals:
 
+li $t2, 0
+addi $t2, $s1, 0
+li $t7, 0
+li $t0, 0
+li $t1, 0
 loop:
-
     lb $t1, 0($t2)
-    li $v0, 11
-    add $a0, $t1, $zero
     beq $t1, $zero, exit_loop
-    syscall
+if_a:
     li $t6, 'a'
     beq $t1, $t6, inc
+if_e:
     li $t6, 'e'
     beq $t1, $t6, inc
+if_i:
     li $t6, 'i'
     beq $t1, $t6, inc
+if_o:
     li $t6, 'o'
     beq $t1, $t6, inc
+if_u:
     li $t6, 'u'
     beq $t1, $t6, inc
     j end_ifs
@@ -29,17 +36,16 @@ inc:
     addi $t7, $t7, 1
 
 end_ifs:
-    addi $t2, $t2, 1
-    addi $t0, $t0, 1
-    j loop
+
+addi $t2, $t2, 1
+addi $t0, $t0, 1
+
+j loop
 
 exit_loop:
-    li $v0, 1
-    add $a0, $t7, $zero
-    syscall  
 
-    li $v0, 10
-    syscall
+move $s2, $t7
+jr $ra
 
 main:
     li $v0, 4
@@ -53,9 +59,15 @@ main:
 
     move $s3, $a1
     
-    li $t2, 0
-    addi $t2, $s1, 0
-    li $t7, 0
-    li $t0, 0
-    move $t4, $s3
-    j loop
+    jal countVocals  
+
+    li $v0, 4
+    la $a0, str2
+    syscall
+
+    li $v0, 1
+    move $a0, $s2
+    syscall
+
+    li $v0, 10
+    syscall
